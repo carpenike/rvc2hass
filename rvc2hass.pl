93c2f370100877f70e53e145bf6115c4391cc72f
Ryan Holt <ryan@ryanholt.net>
2024-08-20 16:21:59 -0400

    last_will

--- rvc2hass.pl
+++ rvc2hass.pl
@@ -98,7 +98,7 @@ sub initialize_mqtt {
             log_to_journald("MQTT login successful.");
 
             # Set Last Will and Testament (LWT) for availability
-            $mqtt->set_will("rvc2hass/status", "offline", retain => 1, qos => 1);
+            $mqtt->last_will("rvc2hass/status", "offline", 1);  # Set LWT with topic, message, and retain flag
             log_to_journald("LWT set to 'offline' on rvc2hass/status");
 
             # Publish "online" status after successful connection
