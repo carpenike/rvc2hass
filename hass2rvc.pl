bb387c2b5457661d0cb4400c00281d2b7fcada3e
Ryan Holt <ryan@ryanholt.net>
2024-08-23 17:58:02 -0400

    disable debug

--- hass2rvc.pl
+++ hass2rvc.pl
@@ -15,7 +15,7 @@ use threads;
 use threads::shared;
 
 # Command-line options for debugging and log level
-my $debug = 1;
+my $debug = 0;
 my $log_level = LOG_INFO;
 GetOptions("debug" => \$debug, "log-level=i" => \$log_level);
 
